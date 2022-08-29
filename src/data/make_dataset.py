bf216f4229f5be0265bb5e115892a5884b1084c3
Nelson Shilman <nelsonshilman@Nelsons-Air.lan>
2022-08-29 17:55:22 +0300

    complete make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,47 @@
 # -*- coding: utf-8 -*-
-import click
+# import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+
+def main(input_filepaths, output_filepaths):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    #set names
+    df_names = ["age", "workclass", "fnlwgt", "education", "education-num",
+    "marital_status", "occupation", "relationship", "race", "sex",
+    "capital_gain", "capital_loss", "hours_per_week", "native_country", "income"]
+
+    # read files and make train and test dataframes
+    train_df = pd.read_csv(input_filepaths[0],
+        names = df_names)
+
+    test_df = pd.read_csv(input_filepaths[1],
+        names = df_names, skiprows = 1)
+
+    # write files into parquets
+
+    train_df.to_parquet(output_filepaths[0])
+    test_df.to_parquet(output_filepaths[1])
+
+
+ 
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(input_filepaths=[
+            "/home/jovyan/work/github_projs/citiproj/citi-documentation-test/data/raw/adult.data",
+            "/home/jovyan/work/github_projs/citiproj/citi-documentation-test/data/raw/adult.test"],
+        output_filepaths= [
+            "/home/jovyan/work/github_projs/citiproj/citi-documentation-test/data/interim/train_df.parquet",
+            "/home/jovyan/work/github_projs/citiproj/citi-documentation-test/data/interim/test_df.parquet"
+      ])
